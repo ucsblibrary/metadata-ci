f4a9e40939eeef0c40c41a489ce2673445be2f3d
Alex Dunn <dunn.alex@gmail.com>
2017-06-28 18:05:22 -0700

    [rubocop] fix Metrics violations in Check::Entities

--- lib/check/entities.rb
+++ lib/check/entities.rb
@@ -18,29 +18,36 @@ module Check
       decoder = HTMLEntities::Decoder.new("expanded")
 
       files.flatten.map do |file|
-        lines = File.read(file, encoding: "bom|UTF-8").split("\n")
-
-        lines.map.with_index do |line, i|
-          matches = line.scan(decoder.send(:entity_regexp))
-          next if matches.nil?
-
-          matches.map do |match|
-            # There are three types of HTML entities, so the match is
-            # an Array with three elements, @see
-            # https://github.com/threedaymonk/htmlentities/blob/master/lib/htmlentities/decoder.rb#L35
-            match.map.with_index do |pattern, pos|
-              next if pattern.nil?
-
-              entity = format(ENTITY_PATTERNS[pos.to_s], str: pattern)
-
-              EncodedEntity.new(
-                "#{file}:\n  "\
-                "HTML-encoded character '#{entity}' on line #{i + 1}"
-              )
-            end
-          end
-        end
+        scan_file(file, decoder)
       end.flatten.compact.select { |r| r.is_a? EncodedEntity }
+    end
+
+    # @param [String] file
+    # @param [HTMLEntities::Decoder] decoder
+    # @return [Array]
+    def self.scan_file(file, decoder)
+      File.read(file).split("\n").map.with_index do |line, i|
+        matches = line.scan(decoder.send(:entity_regexp))
+        next if matches.nil?
+
+        matches.map do |match|
+          EncodedEntity.new(
+            "#{file}:\n  "\
+            "HTML-encoded character '#{format_match(match)}' on line #{i + 1}"
+          )
+        end
+      end
+    end
+
+    def self.format_match(match)
+      # There are three types of HTML entities, so the match is
+      # an Array with three elements, two of them nil; @see
+      # https://github.com/threedaymonk/htmlentities/blob/master/lib/htmlentities/decoder.rb#L35
+      match.map.with_index do |pattern, pos|
+        next if pattern.nil?
+
+        break format(ENTITY_PATTERNS[pos.to_s], str: pattern)
+      end
     end
   end
 end
