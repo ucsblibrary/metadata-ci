8c44f3776aa9d29197354e15907e1eb8b2f96414
Alex Dunn <dunn.alex@gmail.com>
2017-06-29 14:38:14 -0700

    [test] combine Check::Date test files

--- test/date_test.rb
+++ test/date_test.rb
@@ -3,7 +3,28 @@
 require "minitest/autorun"
 require File.expand_path("../../lib/check.rb", __FILE__)
 
-class CsvTest < MiniTest::Test
+class DateTest < MiniTest::Test
+  def test_mods_fields
+    refute_empty Check::Date.mods_fields
+    assert(Check::Date.mods_fields.all? { |c| c.is_a? String })
+  end
+
+  def test_mods_checking
+    assert_empty(
+      Check::Date.mods(
+        File.expand_path("../fixtures/mods/cusbmss228-p00001.xml", __FILE__)
+      )
+    )
+
+    errors = Check::Date.mods(
+      File.expand_path("../fixtures/mods/cusbmss228-p00001-invalid.xml",
+                       __FILE__)
+    )
+    refute_empty errors
+    assert_equal 1, errors.count
+    assert(errors.all? { |e| e.is_a? InvalidDate })
+  end
+
   def test_csv_columns
     refute_empty Check::Date.csv_columns
     assert(Check::Date.csv_columns.all? { |c| c.is_a? Symbol })
