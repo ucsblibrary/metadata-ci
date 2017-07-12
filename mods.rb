cc7e4411ba6a49c29d1b145a39b12a261180c915
Alex Dunn <dunn.alex@gmail.com>
2017-07-12 09:56:06 -0700

    MODS: import type parsing from ADRL

--- mods.rb
+++ mods.rb
@@ -2,6 +2,61 @@
 
 module Fields
   module MODS
+    # @param [Mods::Record] mods
+    # @return [Hash]
+    def self.resource_type(mods)
+      if collection?(mods)
+        # This ensures that MODS Collections have their work_type be
+        # Collection /and/ all the formats of the items they contain
+        RESOURCE_MAP['collection'].merge(
+          uri: [
+            RESOURCE_MAP['collection'][:uri],
+            *mods.typeOfResource.content.map { |t| RESOURCE_MAP[t][:uri] }
+          ].flatten
+        )
+      else
+        # return an empty hash in the case of XML fragments
+        RESOURCE_MAP[mods.typeOfResource.content.first] || {}
+      end
+    end
+
+    # @param [Mods::Record] mods
+    def self.model(mods)
+      return 'Collection' if collection?(mods)
+
+      case mods.typeOfResource.content.first
+      when *ETD_TYPES
+        'ETD'
+      when *AUDIO_TYPES
+        'AudioRecording'
+      when *IMAGE_TYPES
+        'Image'
+      else
+        raise ArgumentError,
+              "Unsupported work type #{mods.typeOfResource.content.first}"
+      end
+    end
+
+    # This is more complicated than other object types because a
+    # collection will also specify the types of the objects within it
+    #
+    # @param [Mods::Record] mods
+    def self.collection?(mods)
+      type_keys = mods.typeOfResource.attributes.map(&:keys).flatten
+      return false unless type_keys.include?('collection')
+
+      mods.typeOfResource.attributes.any? do |hash|
+        hash.fetch('collection').value == 'yes'
+      end
+    end
+
+    AUDIO_TYPES = [
+      'sound recording-musical',
+      'sound recording-nonmusical'
+    ].freeze
+    ETD_TYPES = %w[text].freeze
+    IMAGE_TYPES = ['still image'].freeze
+
     # Mapping of MODS typeOfResource to URIs
     # https://help.library.ucsb.edu/browse/DIGREPO-522
     # https://www.loc.gov/standards/mods/userguide/typeofresource.html
